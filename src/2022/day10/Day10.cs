2bee5a46fc4a8c06addcc8003d6a749b612064d7
S. Kyle Korndoerfer <kylekorndoerfer@outlook.com>
2022-12-11 23:57:40 -0500

    2022 Day 10 Part 2

--- src/2022/day10/Day10.cs
+++ src/2022/day10/Day10.cs
@@ -15,20 +15,17 @@ public class Day10 : IPuzzle
 		Console.WriteLine("\n>> Day 10 - Cathode-Ray Tube");
 		_data = File.ReadAllLines(Path.Combine(dataDirectory, DataFile));
 
-		Puzzle1();
-		// Console.WriteLine($"   Puzzle 2: Visited locations = {_longVisited.Count}");
-	}
-
-	void Puzzle1()
-	{
 		int cycle = 0;
 		int regX = 1;
 		Dictionary<int, int> registerHistory = new Dictionary<int, int>();
+		bool[,] display = new bool[6,40];
 
 		foreach (string line in _data)
 		{
 			cycle++;
 			registerHistory.Add(cycle, regX);	// cycle
+			UpdateDisplay(display, cycle, regX);
+
 
 			if (line == "noop")
 			{
@@ -40,6 +37,7 @@ public class Day10 : IPuzzle
 				// Operation; 2 cycles (1 cycle already done)
 				cycle++;
 				registerHistory.Add(cycle, regX);	// cycle 2 of 2
+				UpdateDisplay(display, cycle, regX);
 			}
 
 			regX += Int32.Parse(line.Split(" ")[1]);
@@ -51,6 +49,7 @@ public class Day10 : IPuzzle
 		// 	Console.WriteLine($"Cycle {key}; Register: {val}");
 		// }
 
+		// Part 1 answer
 		int sum = 0;
 		for (int i = 20; i <= 220; i += 40)
 		{
@@ -59,5 +58,28 @@ public class Day10 : IPuzzle
 		}
 
 		Console.WriteLine($"   Puzzle 1: Signal Strength Sum = {sum}");
+
+		// Part 2 answer = PGHFGLUG
+		Console.WriteLine("   Puzzle 2:");
+		for (int i = 0; i < 6; i++)
+		{
+			Console.Write("\t");
+			for (int j = 0; j < 40; j++)
+			{
+				Console.Write(display[i, j] ? "#" : ".");
+			}
+			Console.Write(Environment.NewLine);
+		}
+	}
+
+	void UpdateDisplay(bool[,] display, int cycle, int regX)
+	{
+		int row = (cycle - 1) / 40;
+		int hPos = (cycle -1) % 40;
+
+		if (hPos >= regX - 1 && hPos <= regX + 1)
+		{
+			display[row, hPos] = true;
+		}
 	}
 }
