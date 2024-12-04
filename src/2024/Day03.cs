3c074488a6930534e76dc2dea408d5b7e903024d
Kyle Korndoerfer <kyle.korndoerfer@outlook.com>
2024-12-03 23:28:08 -0500

    2024 Day 03; part 2

--- src/2024/Day03.cs
+++ src/2024/Day03.cs
@@ -6,6 +6,11 @@ using AdventOfCode;
 
 internal class Day03 : PuzzleBase
 {
+    private const string MulPattern = @"mul\(\d{1,3},\d{1,3}\)";
+    private const string OperandsPattern = @"\d{1,3}";
+    private const string DoPattern = @"do\(\)";
+    private const string DontPattern = @"don't\(\)";
+        
     private string[] _data;
 
     public Day03(int year, Downloader downloader) : base(year, downloader)
@@ -17,6 +22,7 @@ internal class Day03 : PuzzleBase
     {
         _data = await Downloader
             //.GetInput(Year, 3, "test.txt")
+            //.GetInput(Year, 3, "test2.txt")
             .GetInput(Year, 3)
             .ConfigureAwait(false);
 
@@ -26,16 +32,13 @@ internal class Day03 : PuzzleBase
 
     private void Puzzle1()
     {
-        var finder = new Regex(@"mul\(\d{1,3},\d{1,3}\)");
-        var operands = new Regex(@"\d{1,3}");
-
         var sum = 0;
         foreach (var line in _data)
         {
-            var matches = finder.Matches(_data[0]);
+            var matches = Regex.Matches(line, MulPattern);
             for (var i = 0; i < matches.Count; i++)
             {
-                var numbers = operands.Matches(matches[i].Value);
+                var numbers = Regex.Matches(matches[i].Value, OperandsPattern);
                 sum += Int32.Parse(numbers[0].Value) * Int32.Parse(numbers[1].Value);
             }
         }
@@ -45,6 +48,36 @@ internal class Day03 : PuzzleBase
 
     private void Puzzle2()
     {
-        Utils.WriteResults($"Puzzle 2: ");
+        var sum = 0;
+        var enabled = true;
+        foreach (var line in _data)
+        {
+            var matches = new List<Match>();
+            matches.AddRange(Regex.Matches(line, MulPattern));
+            matches.AddRange(Regex.Matches(line, DoPattern));
+            matches.AddRange(Regex.Matches(line, DontPattern));
+            
+            foreach (var item in matches.OrderBy(m => m.Index).ToList())
+            {
+                if (item.Value == "do()")
+                {
+                    enabled = true;
+                }
+                else if (item.Value == "don't()")
+                {
+                    enabled = false;
+                }
+                else 
+                {
+                    if (enabled)
+                    {
+                        var numbers = Regex.Matches(item.Value, OperandsPattern);
+                        sum += Int32.Parse(numbers[0].Value) * Int32.Parse(numbers[1].Value);
+                    }
+                }
+            }
+        }
+        
+        Utils.WriteResults($"Puzzle 2: {sum}");
     }
 }
