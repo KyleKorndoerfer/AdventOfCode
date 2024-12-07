f9daa0285f0d3f157dc39f52ce07c903eb7737b4
Kyle Korndoerfer <kyle.korndoerfer@outlook.com>
2024-12-07 17:40:57 -0500

    2024 Day 05; part 2 (refactored after looking for help on part 2)

--- src/2024/Day05.cs
+++ src/2024/Day05.cs
@@ -1,3 +1,5 @@
+using System.Collections.Immutable;
+
 namespace AdventOfCode2024;
 
 using AdventOfCode;
@@ -6,7 +8,7 @@ internal class Day05 : PuzzleBase
 {
     private string[] _data;
 
-    private Dictionary<int, List<int>> _rules = new();
+    private readonly HashSet<(int, int)> _rules = new();
     private int _updatesStartingRow;
     
     public Day05(int year, Downloader downloader) : base(year, downloader)
@@ -18,70 +20,19 @@ internal class Day05 : PuzzleBase
     {
         _data = await Downloader
             //.GetInput(Year, 5, "test.txt")
-             .GetInput(Year, 5)
+            .GetInput(Year, 5)
             .ConfigureAwait(false);
 
         LoadRules();
+        (var validSum, var invalidSum) = ProcessReports();
         
-        Puzzle1();
-        Puzzle2();
-    }
-
-    private void Puzzle1()
-    {
-        int sum = 0;
-        for (var i = _updatesStartingRow; i < _data.Length; i++)
-        {
-            var valid = true;
-            //Utils.WriteDebug($"Pages: {_data[i]}");
-            var _pages = _data[i].Split(',').Select(Int32.Parse).ToArray();
-            for (var y = 0; y < _pages.Length - 1; y++)
-            {
-                //Utils.WriteDebug($"\tChecking Page '{_pages[y]}'");
-                if (_rules.ContainsKey(_pages[y]))
-                {
-                    var rule = _rules[_pages[y]];
-                    for (var j = y + 1; j < _pages.Length; j++)
-                    {
-                        valid = rule.Contains(_pages[j]);
-                        //Utils.WriteDebug($"\t\tDoes page '{_pages[y]}' come before page '{_pages[j]}'?  {valid}");
-                        if (!valid)
-                        {
-                            break;
-                        }
-                    }
-                }
-                else
-                {
-                    //Utils.WriteDebug($"\t\tNo rule for for page '{_pages[y]}'");
-                    valid = false;
-                }
-
-                if (!valid)
-                {
-                    break;
-                }
-            }
-            
-            if (valid)
-            {
-                //Utils.WriteDebug($"\tValid; Middle page # = {_pages[_pages.Length / 2]}");
-                sum += _pages[_pages.Length / 2];
-            }
-        }
-        
-        Utils.WriteResults($"Puzzle 1: {sum}");
-    }
-
-    private void Puzzle2()
-    {
-        
-        Utils.WriteResults($"Puzzle 2: ");
+        Utils.WriteResults($"Puzzle 1: {validSum}");
+        Utils.WriteResults($"Puzzle 2: {invalidSum}");
     }
 
     private void LoadRules()
     {
-        for (int i = 0; i < _data.Length; i++)
+        for (var i = 0; i < _data.Length; i++)
         {
             if (string.IsNullOrEmpty(_data[i]))
             {
@@ -90,15 +41,32 @@ internal class Day05 : PuzzleBase
             }
             
             var split = _data[i].Split('|').Select(Int32.Parse).ToArray();
+            _rules.Add((split[0], split[1]));
+        }
+    }
 
-            if (_rules.ContainsKey(split[0]))
+    private (int validSum, int invalidSum) ProcessReports()
+    {
+        var validSum = 0;
+        var invalidSum = 0;
+        
+        for (var i = _updatesStartingRow; i < _data.Length; i++)
+        {
+            var pages = _data[i].Split(',').Select(Int32.Parse).ToArray();
+
+            var valid = pages.Zip(pages[1..], (x, y) => _rules.Contains((x, y))).All(r => r);
+
+            if (valid)
             {
-                _rules[split[0]].Add(split[1]);
+                validSum += pages[pages.Length / 2];
             }
             else
             {
-                _rules.Add(split[0], new List<int> { split[1] });
+                Array.Sort(pages, (x, y) => _rules.Contains((x, y)) ? -1 : 1);
+                invalidSum += pages[pages.Length / 2];
             }
         }
+        
+        return (validSum, invalidSum);
     }
 }
